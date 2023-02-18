9a93c3bcda389c47a955f05eaea7a386aa3e06b4
EzeBrito <ezequielsbrito@gmail.com>
2023-02-18 05:16:34 -0300

    Dockerfile

--- BackEnd/src/main/java/com/portfolio/EzeBrito/Entity/Persona.java
+++ BackEnd/src/main/java/com/portfolio/EzeBrito/Entity/Persona.java
@@ -25,17 +25,33 @@ import lombok.Setter;
 public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     
     @NotNull
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    @Size(min = 1, max = 50, message = "No cumple con la longitud.")
     private String nombre;
     
     @NotNull
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    @Size(min = 1, max = 50, message = "No cumple con la longitud.")
     private String apellido;
     
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    /*@Size(min = 1, max = 50, message = "No cumple con la longitud.")*/
     private String img;
+    
+    @NotNull
+    private String descripcion;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String descripcion, String img) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.descripcion = descripcion;
+        this.img = img;
+    }
+    
+    
+    
     
 }
